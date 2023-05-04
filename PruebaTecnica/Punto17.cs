3aebeaf92998cf7d0e6db2ee9d58b71453c96b99
AlejooGutierrez <daleya505@gmail.com>
2023-05-04 00:45:15 -0500

    .

--- PruebaTecnica/Punto17.cs
+++ PruebaTecnica/Punto17.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PruebaTecnica
+﻿namespace PruebaTecnica
 {
     public class Punto17
     {
