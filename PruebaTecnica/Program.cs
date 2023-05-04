816559a9d317009a54139532239bfd4f71e681c9
AlejooGutierrez <daleya505@gmail.com>
2023-05-04 03:08:12 -0500

    prueba completada

--- PruebaTecnica/Program.cs
+++ PruebaTecnica/Program.cs
@@ -1,7 +1,9 @@
 ﻿using PruebaTecnica;
+using static PruebaTecnica.Punto19;
 
 
-Console.WriteLine("Testing Punto 17 \n");
+
+Console.WriteLine("\nTesting Punto 17 \n");
 Console.WriteLine(Punto17.Dividir(5,3));
 
 
@@ -19,4 +21,88 @@ Console.WriteLine(Punto18.ObtenerBase(287, 9));
 Console.WriteLine(Punto18.ObtenerBase(287, 10));
 
 
+
+
+Console.WriteLine("\nTesting Punto 19 \n");
+
+//Forma uno agregando directamenete el arbol /*No funcionaría de forma adecuada esta forma ya que no se puede agregar un subArbol a un subArbol ya creado*/
+Console.WriteLine("Forma 1");
+
+Arbol f1Arbol = new Arbol(10);
+Arbol f1Arbol2 = new Arbol(7);
+Arbol f1Arbol3 = new Arbol(5,16);
+
+f1Arbol.AgregarSubArbol(new Arbol(2));
+f1Arbol2.AgregarSubArbol(new Arbol(3));
+f1Arbol3.AgregarSubArbol(new Arbol(1,165));
+
+double pesoTotal1 = f1Arbol.CalcularPeso();
+Console.WriteLine("El peso total del árbol 1 forma 1 es: " + pesoTotal1);
+Console.WriteLine("La áltura del árbol 1 forma 1 es: " + f1Arbol.ObtenerAltura());
+Console.WriteLine("La áltura del subarbol del árbol 1 forma 1 es: " + f1Arbol.subarboles[0].ObtenerAltura());
+Console.WriteLine("El promedio del peso del árbol 1 forma 1 es: " + f1Arbol.CalcularPesoPromedio());
+
+
+double pesoTotal2 = f1Arbol2.CalcularPeso();
+Console.WriteLine("El peso total del árbol 2 forma 1 es: " + pesoTotal2);
+Console.WriteLine("La áltura del árbol 2 forma 1 es: " + f1Arbol2.ObtenerAltura());
+Console.WriteLine("La áltura del subarbol del árbol 2 forma 1 es: " + f1Arbol2.subarboles[0].ObtenerAltura());
+Console.WriteLine("El promedio del peso del árbol 2 forma 1 es: " + f1Arbol2.CalcularPesoPromedio());
+
+
+double pesoTotal3 = f1Arbol3.CalcularPeso();
+Console.WriteLine("El peso total del árbol 3 forma 1 es: " + pesoTotal3);
+Console.WriteLine("La áltura del árbol 3 forma 1 es: " + f1Arbol3.ObtenerAltura());
+Console.WriteLine("La áltura del subarbol del árbol 3 forma 1 es: " + f1Arbol3.subarboles[0].ObtenerAltura());
+Console.WriteLine("El promedio del peso del árbol 3 forma 1 es: " + f1Arbol3.CalcularPesoPromedio());
+
+
+//Forma dos, creando los subarboles primero y luego relacionarlos al arbol padre. Aquí es posible agregar los niveles de subarboles necesarios
+Console.WriteLine("\nForma 2");
+Arbol f2Arbol = new Arbol(10);
+Arbol f2Arbol2 = new Arbol(7);
+Arbol f2Arbol3 = new Arbol(5);
+
+Arbol f2SubArbol = new Arbol(5,45);
+Arbol f2SubArbol2 = new Arbol(10,235);
+Arbol f2SubArbol3 = new Arbol(7,454);
+
+Arbol f2SubSubArbol = new Arbol(50, 450);
+Arbol f2SubSubArbol2 = new Arbol(100, 2035);
+Arbol f2SubSubArbol3 = new Arbol(70, 4054);
+
+f2Arbol.AgregarSubArbol(f2SubArbol);
+f2Arbol2.AgregarSubArbol(f2SubArbol2);
+f2Arbol3.AgregarSubArbol(f2SubArbol3);
+
+f2SubArbol.AgregarSubArbol(f2SubSubArbol);
+f2SubArbol2.AgregarSubArbol(f2SubSubArbol2);
+f2SubArbol3.AgregarSubArbol(f2SubSubArbol3);
+
+double f2pesoTotal1 = f2Arbol.CalcularPeso();
+Console.WriteLine("El peso total del árbol 1 forma 2 es: " + f2pesoTotal1);
+Console.WriteLine("La áltura del árbol 1 forma 2 es: " + f2Arbol.ObtenerAltura());
+Console.WriteLine("La áltura del subarbol del árbol 1 forma 2 es: " + f2Arbol.subarboles[0].ObtenerAltura());
+Console.WriteLine("El promedio del peso del árbol 1 forma 2 es: " + f2Arbol.CalcularPesoPromedio());
+
+
+double f2pesoTotal2 = f2Arbol2.CalcularPeso();
+Console.WriteLine("El peso total del árbol 2 forma 2 es: " + f2pesoTotal2);
+Console.WriteLine("La áltura del árbol 2 forma 2 es: " + f2Arbol2.ObtenerAltura());
+Console.WriteLine("La áltura del subarbol del árbol 2 forma 2 es: " + f2Arbol2.subarboles[0].ObtenerAltura());
+Console.WriteLine("El promedio del peso del árbol 2 forma 2 es: " + f2Arbol2.CalcularPesoPromedio());
+
+
+double f2pesoTotal3 = f2Arbol3.CalcularPeso();
+Console.WriteLine("El peso total del árbol 3 forma 2 es: " + f2pesoTotal3);
+Console.WriteLine("La áltura del árbol 3 forma 2 es: " + f2Arbol3.ObtenerAltura());
+Console.WriteLine("La áltura del subarbol del árbol 3 forma 2 es: " + f2Arbol3.subarboles[0].ObtenerAltura());
+Console.WriteLine("El promedio del peso del árbol 3 forma 2 es: " + f2Arbol3.CalcularPesoPromedio());
+
+
+//double pesoTotal = arbol.CalcularPeso();
+//Console.WriteLine("El peso total del árbol es: " + pesoTotal);
+
+
 Console.ReadLine();
+
---
+++ PruebaTecnica/Punto19.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica
+{
+    ///Casos de prueba en clase Program
+    public class Punto19
+    {
+
+        public class Arbol
+        {
+            //Variables
+            double pesoDelArbol;
+            int alturaDelArbol = 0;
+            Arbol raizDeDondeViene = null;
+            public List<Arbol> subarboles;
+
+            //Constructores
+            public Arbol(int peso)
+            {
+                pesoDelArbol = peso;
+                subarboles = new List<Arbol>();
+            }
+            public Arbol(int peso, int altura)
+            {
+                pesoDelArbol = peso;
+                subarboles = new List<Arbol>();
+                alturaDelArbol = altura;
+            }
+
+            //Metodos
+            public int ObtenerAltura()
+            {
+                //En caso de no tener altura asignada retorna 0
+                if (alturaDelArbol == 0) return 0;
+                return this.alturaDelArbol;
+            }
+            public void AgregarSubArbol(Arbol subarbol)
+            {
+                //Agrega el arból papá a la raíz del hijo 
+                subarbol.raizDeDondeViene = this;
+                subarboles.Add(subarbol);
+            }
+
+            public double CalcularPeso()
+            {
+                double pesoTotal = this.pesoDelArbol;
+
+                //Recorre los subarboles encontrados y retorna la sumatoria total encontrada y de forma recursiva usa este mismo metodo con los subarboles de los subarboles encontrados
+                foreach (Arbol subarbol in this.subarboles)
+                {
+                    pesoTotal += subarbol.CalcularPeso();
+                }
+                return pesoTotal;
+            }
+
+            public double CalcularPesoPromedio()
+            {
+                double pesoTotal = this.pesoDelArbol;
+                int count = 1;
+
+                //similar a la función anterior, solo que suma la cantidad de iteraciones para saber cuantos arboles y subarboles fuerón tenidos en cuenta para la suma y así sacar el promedio
+                foreach (Arbol subarbol in this.subarboles)
+                {
+                    pesoTotal += subarbol.CalcularPesoPromedio();
+                    count++;
+                }
+                return pesoTotal / count;
+            }
+        }
+
+
+    }
+}
