0b84720f35535e572578e2c5940b47698af44694
AlejooGutierrez <daleya505@gmail.com>
2023-05-04 01:50:27 -0500

    punto 18

--- PruebaTecnica/Program.cs
+++ PruebaTecnica/Program.cs
@@ -1,6 +1,22 @@
 ﻿using PruebaTecnica;
 
 
-//Punto 17
+Console.WriteLine("Testing Punto 17 \n");
 Console.WriteLine(Punto17.Dividir(5,3));
 
+
+
+Console.WriteLine("\nTesting Punto 18 \n");
+Console.WriteLine(Punto18.ObtenerBase(287, 1));
+Console.WriteLine(Punto18.ObtenerBase(287, 2));
+Console.WriteLine(Punto18.ObtenerBase(287, 3));
+Console.WriteLine(Punto18.ObtenerBase(287, 4));
+Console.WriteLine(Punto18.ObtenerBase(287, 5));
+Console.WriteLine(Punto18.ObtenerBase(287, 6));
+Console.WriteLine(Punto18.ObtenerBase(287, 7));
+Console.WriteLine(Punto18.ObtenerBase(287, 8));
+Console.WriteLine(Punto18.ObtenerBase(287, 9));
+Console.WriteLine(Punto18.ObtenerBase(287, 10));
+
+
+Console.ReadLine();
---
+++ PruebaTecnica/Punto18.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Text;
+
+namespace PruebaTecnica
+{
+    public class Punto18
+    {
+        public static string ObtenerBase(int numero, int baseAConvertir)
+        {
+            //Guarda el valor inicial 
+            int valorInicial = numero;
+
+            StringBuilder resultado = new StringBuilder();
+
+            if (baseAConvertir < 2) return "El número ingresado debe ser mayor a 1";
+            else if (baseAConvertir == 2) return valorInicial + " en base " + baseAConvertir + " es: " + Convert.ToString(numero, 2);
+
+            //Guarda en un StringBuilder el residuo y le cambia el valor a la varia a con el cociente para retornar el while.
+            while (numero % baseAConvertir != 0)
+            {
+                resultado.Append(numero % baseAConvertir);
+                numero = numero / baseAConvertir;
+            }
+
+            //Despues de que la divison entre el número y la base de uno, guarda el residuo y el cociente
+            resultado.Append(numero % baseAConvertir);
+            resultado.Append(numero / baseAConvertir);
+
+            //el valor guardado en "resultado" lo devuelve en reversa ya que ese es el resultado del número convertido a la base ingresada
+            char[] chars = resultado.ToString().ToCharArray();
+            Array.Reverse(chars);
+            string valorEnReversa = new string(chars);
+
+            //Texto de retorno.
+            return valorInicial + " en base " + baseAConvertir + " es: " + int.Parse(valorEnReversa);
+        }
+    }
+}
